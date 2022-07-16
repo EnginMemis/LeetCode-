0f8794eead5347e79fb65b37c8c390cd02e35b21
Engin <82648004+EnginMemis@users.noreply.github.com>
2022-07-16 22:12:34 +0300

    Update 26. Remove Duplicates from Sorted Array.c

--- 26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.c
+++ 26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.c
@@ -1,10 +1,9 @@
 int removeDuplicates(int* nums, int numsSize){
     int i;
     int index;
-    
     int control[201] = {0};
-    
     index = 0;
+    
     for(i = 0; i < numsSize; ++i){
         if(control[ nums[i] + 100] == 0){
             control[ nums[i] + 100 ] = 1;
