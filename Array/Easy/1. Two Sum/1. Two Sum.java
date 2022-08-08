de8cc308272d1af6ca9b55fbeba7e40890026a7f
Engin <82648004+EnginMemis@users.noreply.github.com>
2022-08-08 21:02:47 +0300

    Update 1. Two Sum.java

--- Array/Easy/1. Two Sum/1. Two Sum.java
+++ Array/Easy/1. Two Sum/1. Two Sum.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; ++i){
+            Integer index = map.get(nums[i]);
+            if(index != null)
+                return new int[]{i, index};
+            else
+                map.put(target - nums[i], i);
+        }
+        return nums;
+    }
+}
