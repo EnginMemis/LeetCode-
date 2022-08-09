8e0e46e4a0bd1c505a76a1a1bcef9ae29ef2033e
Engin <82648004+EnginMemis@users.noreply.github.com>
2022-08-09 22:08:42 +0300

    Update 9. Palindrome Number.java

--- Math/Easy/9. Palindrome Number/9. Palindrome Number.java
+++ Math/Easy/9. Palindrome Number/9. Palindrome Number.java
@@ -1,26 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        Stack<Integer> stack = new Stack<Integer>();
-        int number = x;
-        int multiple = 1;
+        int temp = x;
+        int number = 0;
         if(x < 0)
             return false;
-        else if(x == 0)
-            return true;
-        else{
-            while(number != 0){
-                stack.push(number % 10);
-                number /= 10;
-            }
 
-            while(!stack.empty()){
-                number += stack.pop() * multiple;
-                multiple *= 10;
-            }
-            if(number == x)
-                return true;
-            else
-                return false;
+        while(temp != 0){
+            number *= 10;
+            number += temp % 10;
+            temp /= 10;
         }
+        if(number == x)
+            return true;
+        else
+            return false;
     }
 }
